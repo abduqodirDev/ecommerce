dd1188f5cbc53ef376725c1f281d980ece047c2b
abduqodir do'stmurodov <abduqodirdusmurodov@gmail.com>
2024-09-22 14:48:41 +0300

    fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -155,5 +155,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'abduqodirdusmurodov@gmail.com'
-EMAIL_HOST_PASSWORD = 'mhcdkoemjafwqevy'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
